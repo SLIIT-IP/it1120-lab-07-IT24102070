8398acac7bce93a5b79d816a7300ad7252df9958
IT24102070 <IT24102070@my.sliit.lk>
2024-09-23 22:40:28 +0530

    Add files via upload

---
+++ Q2/IT24102070Lab7Q2A.java
@@ -0,0 +1,12 @@
+public class IT24102070Lab7Q2A
+{
+ public static void main(String[] args)
+ {
+  for(int i=1;i<5;i++)
+  {
+   for(int j = 1;j<6;j++) 
+      System.out.print("$ ");
+   System.out.print("\n");
+  }
+ }
+}
